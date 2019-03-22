4f698f55ecc566991d92c11db489ea160c7f3c83
Kheeran <kn16063@my.bristol.ac.uk>
2019-03-22 17:06:55 +0000

    initial commit

---
+++ Cube.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+
+main_array = np.zeros((8,8,8,8,8,8,8,3,3,3,3,3,3,3))
+
+print (main_array)
+
+
+
+#
+# class Cubicle:
+#     def __init__(self, position, orientation):
+#         self.position = position
+#         self.orientation = orientation
+#
+#     def Up(cubicle):
+#         if (0 <= cubicle.position <= 3):
+#             cubicle.position += 1 % 4
+#
+#     def Down(cubicle):
+#         if (4 <= cubicle.position <= 7):
+#             cubicle.position = (cubicle.position + 1) % 4 + 4
+#
+#     def Right(cubicle):
+#         # TO DO
+#
+#     def Left(cubicle):
+#         # TO DO
+#
+#     def Front(cubicle):
+#         #TO DO
+#
+#     def Back(cubicle):
+#         #To DO
