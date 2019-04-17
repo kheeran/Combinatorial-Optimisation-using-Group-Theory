0b68c54d3bfadcb917f60670d0dcf1bd4a4de154
Kheeran <kn16063@bristol.ac.uk>
2019-04-17 18:18:40 +0100

    running with symmery group and cleanup

--- Cube.py
+++ Cube.py
@@ -2,34 +2,54 @@ import numpy as np
 import time as time
 import pickle
 
-def move_up(config):
+def _U(config):
     return config[0:4] + config[7] + config[4:7] + config[8:12] + config[15] + config[12:15]
 
-def move_front(config):
+def _D(config):
+    return config[3] + config [0:3] + config[4:8] + config[11] + config[8:11] + config[12:16]
+
+def _F(config):
     x1 = (int(config[10]) + 2) % 3
     x2 = (int(config[14]) + 1) % 3
     x6 = (int(config[15]) + 2) % 3
     x7 = (int(config[9]) + 1) % 3
     return config[0] + config[2] + config [6] + config[3:6] + config[7] + config [1] + config[8] + str(x1) + str(x2) + config[11:14] + str(x6) + str(x7)
 
-def move_right(config):
+def _B(config):
+    x0 = (int(config[12]) + 1) % 3
+    x3 = (int(config[8]) + 2) % 3
+    x4 = (int(config[13]) + 2) % 3
+    x5 = (int(config[11]) + 1) % 3
+    return config[4] + config [1:3] + config[0] + config[5] + config[3] + config[6:8] + str(x0) + config[9:11] + str(x3) + str(x4) + str(x5) + config[14:16]
+
+def _R(config):
     x2 = (int(config[11]) + 2) % 3
     x3 = (int(config[13]) + 1) % 3
     x5 = (int(config[14]) + 2) % 3
     x6 = (int(config[10]) + 1) % 3
     return config[0:2] + config[3] + config[5] + config[4] + config[6] + config[2] + config[7:10] + str(x2) + str(x3) + config[12] + str(x5) + str(x6) + config[15]
 
-# def basic_moves(node):
-#     return {'U1' : move_up(node), 'U2': move_up(move_up(node)), 'U3': move_up(move_up(move_up(node))), 'F1':move_front(node), 'F2' : move_front(move_front(node)), 'F3' : move_front(move_front(move_front(node))), 'R1' : move_right(node), 'R2' : move_right(move_right(node)), 'R3' : move_right(move_right(move_right(node)))}
+def _L(config):
+    x0 = (int(config[9]) + 2) % 3
+    x1 = (int(config[15]) + 1) % 3
+    x4 = (int(config[8]) + 1) % 3
+    x7 = (int(config[12]) + 2) % 3
+    return config[1] + config[7] + config[2:4] + config[0] + config[5:7] + config[4] + str(x0) + str(x1) + config[10:12] + str(x4) + config[13:15] + str(x7)
 
 def basic_moves(node):
-    return {'U1' : move_up(node), 'U2': move_up(move_up(node)), 'U3': move_up(move_up(move_up(node))), 'F1':move_front(node), 'F2' : move_front(move_front(node)), 'F3' : move_front(move_front(move_front(node)))}
+    return {'U1' : _U(node), 'U2': _U(_U(node)), 'U3': _U(_U(_U(node))), 'F1':_F(node), 'F2' : _F(_F(node)), 'F3' : _F(_F(_F(node))), 'R1' : _R(node), 'R2' : _R(_R(node)), 'R3' : _R(_R(_R(node)))}
+
+def gen_2_moves(node):
+    return {'U1' : _U(node), 'U2': _U(_U(node)), 'U3': _U(_U(_U(node))), 'F1':_F(node), 'F2' : _F(_F(node)), 'F3' : _F(_F(_F(node)))}
+
+def symmetry_moves(node):
+    return {'Rx' : _R(_L(_L(_L(node)))), 'Ry' : _U(_D(_D(_D(node)))), 'Rz' : _F(_B(_B(_B(node))))}
 
 def record(unexplored, visited, equivalence, diameter_count):
     node = unexplored.pop(0)
     diameter = visited[node][2]
     diameter_count[int(diameter)] = diameter_count[int(diameter)] + 1
-    next_configs = basic_moves(node)
+    next_configs = gen_2_moves(node) # basic_moves(node), gen_2_moves(node), symmetry_moves(node)
     for edge in next_configs:
         if visited.get(next_configs[edge]) == None:
             visited[next_configs[edge]] = (node, edge, diameter+1)
@@ -53,9 +73,6 @@ def load_obj(name ):
 
 # MAIN RUN
 
-print("2-Generator group G=<U,F>")
-print("")
-
 # explored = {}
 visited, unexplored = init_dict()
 # unexplored = ['0123456700000000', '0123745600000000', '0263457102100021', '0123745600000000', '0263457102100021']
@@ -66,7 +83,8 @@ diameter_count = np.zeros(20,  np.int32)
 n=0
 start = time.time()
 # goal = 3674160 # G=<U,F,R>
-goal = 29160 # G=<U,F>
+# goal = 29160 # G=<U,F>
+goal = 24 # G = Symmetry
 checkpoint = goal/10
 
 # while n < goal:
@@ -104,21 +122,40 @@ save_obj(visited, "visited")
 
 
 # TESTS
-
+#
 # config = "0124763501210200"
-# if move_up(config) == "0124576301210200":
+# if _U(config) == "0124576301210020":
 #     print ("UP Correct")
 # else:
 #     print ("UP WRONG !!!!")
 #
 # config = "0123456700000000"
-# if move_front(config) == "0263457102100021":
+# if _F(config) == "0263457102100021":
 #     print("FRONT Correct")
 # else:
 #     print("FRONT WRONG !!!!")
 #
 # config = "0263457102100021"
-# if move_right(config) == "0235476102210121":
+# if _R(config) == "0235476102210121":
 #     print("RIGHT Correct")
 # else:
 #     print("RIGHT WRONG !!!!")
+#
+# config = "0263457102100021"
+# if _D(config) == "3026457100210021":
+#     print ("DOWN Correct")
+# else:
+#     print ("DOWN WRONG !!!!")
+#
+# config = "1360457200210021"
+# if _B(config) == "4361507210222221":
+#     print ("BACK Correct")
+# else:
+#     print ("BACK WRONG !!!!")
+#
+# config = "4361507210222221"
+# if _L(config) == "3261407522222221":
+#     print ("LEFT Correct")
+# else:
+#     print ("LEFT WRONG !!!!")
+#     print (_L(config))
